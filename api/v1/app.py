2e8084e0130003a01875447c0aa7a0f7cfb0f86b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2024-01-06 16:27:24 +0000

    arrange import in ascending order

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from flask import Flask
-import os
-from models import storage
 from api.v1.views import app_views
+from flask import Flask
+from models import storage
+import os
 
 
 app = Flask(__name__)
