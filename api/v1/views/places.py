41e6ac3dadc4dd62de2ebdeddcd5a31cc8cdeb6d
Timishot <timidotcom58@gmail.com>
2024-01-08 21:21:31 +0100

    Fix place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,79 +9,88 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
 
 
-@app_views.route('/cities/<city_id>/places',  methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET'],
+                 strict_slashes=False)
 def city_Place(city_id):
     """returns count of classes user!"""
-    city = storage.all(City)
-    for _, city_obj in city.items():
-        for place in city_obj.places:
-            return jsonify(places.to_dict())
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    places = [place.to_dict() for place in city.places]
+    return jsonify(places)
 
 
-@app_views.route('/users/<user_id>',  methods=['GET'], strict_slashes=False)
-def get_users_id(user_id):
-    """returns count of classes!"""
-    user = storage.all(User)
-    for _, user_obj in user.items():
-         if user_obj.id == user_id:
-             return jsonify(user_obj.to_dict())
-    return abort(404)
+@app_views.route('/places/<place_id>',  methods=['GET'], strict_slashes=False)
+def get_place_id(place_id):
+    """"returns a place object by id"""
+    review_obj = storage.get(Place, place_id)
+    if not review_obj:
+        abort(404)
+    return jsonify(review_obj.to_dict())
 
 
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place_id(place_id):
+    """Deletes a Place object by ID."""
+    place = storage.get(Place, place_id)
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user_id(user_id):
-    """Deletes a State object by ID."""
-    user = storage.get(User, user_id)
+    if not place:
+        abort(404)
 
-    if not user:
-         abort(404)
-
-    storage.delete(user)
+    storage.delete(place)
     storage.save()
 
     return make_response(jsonify({}), 200)
 
 
-
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-def users_create(state_id):
-    """Creates a new user object."""
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'],
+                 strict_slashes=False)
+def place_create(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
     data = request.get_json()
     if not data:
-        return jsonify({"error": "Not a JSON"}), 400
+        abort(400, description="Not a JSON")
 
-    if 'email' not in data:
-        return jsonify({"eriror": "Missing email"}), 400
-    if 'password' not in data:
-        return jsonify({"error": "Missing password"}), 400
-
-    new_user = User(**data)
-    storage.new(new_user)
-    storage.save()
-    return jsonify(new_user.to_dict()), 201
-
-
-@app_views.route('/user/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user_id(user_id):
-    """
-    Updates a User base on user_id
-    """
-    user = storage.get(User, user_id)
-
+    if 'user_id' not in data:
+        return jsonify({"error": "Missing user_id"}), 400
+    user = storage.get(User, data['user_id'])
     if not user:
+        abort(404)
+    if 'name' not in data:
+        return jsonify({"error": "Missing text"}), 400
+
+    data['city_id'] = city_id
+
+    new_place = Place(**data)
+    storage.new(new_place)
+    storage.save()
+    return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_user_id(place_id):
+    """
+    Updates a Place base on user_id
+    """
+    place = storage.get(Place, place_id)
+
+    if not place:
         abort(404)
 
     if not request.get_json():
         abort(400, description="Not a JSON")
 
-    ignore = ['id', 'email', 'created_at', 'updated_at']
+    ignore = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
 
     data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
-            setattr(user, key, value)
+            setattr(Place, key, value)
     storage.save()
-    return make_response(jsonify(user.to_dict()), 200)
-
-
+    return make_response(jsonify(place.to_dict()), 200)
